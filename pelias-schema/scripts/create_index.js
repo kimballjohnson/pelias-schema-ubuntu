ca2d7bff2503dab8655a657aeecfd916870b7b58
Michal Migurski <mike-launchpad@teczno.com>
2016-05-10 20:30:44 -0700

    Killed pelias-schema

--- pelias-schema/scripts/create_index.js
+++
@@ -1,8 +0,0 @@
-
-var client = require('pelias-esclient')(),
-    schema = require('../schema');
-
-client.indices.create( { index: 'pelias', body: schema }, function( err, res ){
-  console.log( '[put mapping]', '\t', 'pelias', err || '\t', res );
-  process.exit( !!err );
-});
