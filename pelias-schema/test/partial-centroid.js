17b975a02aa4b87e0727f60ad5169496a570a418
Michal Migurski <mike-launchpad@teczno.com>
2016-05-10 20:31:42 -0700

    Remade pelias-schema dir for 3.1.0-ubuntu1~trusty1

---
+++ pelias-schema/test/partial-centroid.js
@@ -0,0 +1,60 @@
+
+var schema = require('../mappings/partial/centroid');
+
+module.exports.tests = {};
+
+module.exports.tests.compile = function(test, common) {
+  test('valid schema file', function(t) {
+    t.equal(typeof schema, 'object', 'schema generated');
+    t.equal(Object.keys(schema).length>0, true, 'schema has body');
+    t.end();
+  });
+};
+
+// this should never need to change
+module.exports.tests.type = function(test, common) {
+  test('correct type', function(t) {
+    t.equal(schema.type, 'geo_point', 'correct value');
+    t.end();
+  });
+};
+
+// this should always be enabled for geo_distance filter
+// queries to execute correctly
+module.exports.tests.latlon = function(test, common) {
+  test('latlon enabled', function(t) {
+    t.equal(schema.lat_lon, true, 'correct value');
+    t.end();
+  });
+};
+
+// this should always be enabled for geohash_cell filter
+// queries to execute correctly
+module.exports.tests.geohash = function(test, common) {
+  test('geohash enabled', function(t) {
+    t.equal(schema.geohash, true, 'correct value');
+    t.equal(schema.geohash_prefix, true, 'correct value');
+    t.equal(schema.geohash_precision, 18, 'correct value');
+    t.end();
+  });
+};
+
+// this should be set to 'eager_global_ordinals' in order
+// to build caches at index time rather than query time
+module.exports.tests.fielddata = function(test, common) {
+  test('fielddata configured', function(t) {
+    t.equal(schema.fielddata.loading, 'eager_global_ordinals', 'correct value');
+    t.end();
+  });
+};
+
+module.exports.all = function (tape, common) {
+
+  function test(name, testFunction) {
+    return tape('centroid: ' + name, testFunction);
+  }
+
+  for( var testCase in module.exports.tests ){
+    module.exports.tests[testCase](test, common);
+  }
+};
